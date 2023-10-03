54502be04398e1bfdb12649199d8d195de73dd8c
yuanjun17 <yuanjun918918@gmail.com>
2023-10-03 16:51:55 +0800

    commit

--- My project/Assets/script/walk.cs
+++ My project/Assets/script/walk.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class walk : MonoBehaviour
 {
+    public float walkspeed;
+    private Rigidbody2D myrigidbody;
+    private Animator myanim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myrigidbody = GetComponent<Rigidbody2D>();
+        myanim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Walk();
+        Filp();
+
     }
+    void Walk()
+    {
+        float moveDir = Input.GetAxis("Horizontal");
+        Vector2 playerVel = new Vector2(moveDir * walkspeed,myrigidbody.velocity.y);
+        myrigidbody.velocity = playerVel;
+        bool playerHasXAxisSpeed =Mathf.Abs(myrigidbody.velocity.x) > Mathf.Epsilon;
+        myanim.SetBool("walk", playerHasXAxisSpeed);
+
+    }
+    void Filp()
+    {
+        bool playerHasXAxisSpeed = Mathf.Abs(myrigidbody.velocity.x) >  Mathf.Epsilon;
+        if(playerHasXAxisSpeed)
+        {
+            if(myrigidbody.velocity.x > 0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0,0,0);
+            }
+
+            if (myrigidbody.velocity.x < - 0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+    }
+
+    
 }
+
